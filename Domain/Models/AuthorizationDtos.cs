c7d91823239bee6001b4a1e729ed438378b1d1f2
giogog <ggogoladzee11@gmail.com>
2024-09-29 17:26:15 +0400

    AuthorizationDbo validators

--- Domain/Models/AuthorizationDtos.cs
+++ Domain/Models/AuthorizationDtos.cs
@@ -1,7 +1,31 @@
-﻿namespace Domain.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
-public record RegisterDto(string Username, string Email, string Password);
-public record LoginDto(string Username, string Password);
+namespace Domain.Models;
+
+public record RegisterDto
+{
+    [Required(ErrorMessage = "Username is required.")]
+    public string Username { get; init; }
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email address.")]
+    public string Email { get; init; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    [MinLength(6, ErrorMessage = "Password must be at least 6 characters.")]
+    public string Password { get; init; }
+}
+
+public record LoginDto
+{
+    [Required(ErrorMessage = "Username is required.")]
+    public string Username { get; init; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    public string Password { get; init; }
+}
+
+
 public record LoginResponseDto(Guid Id,string Username, string Token);
 public record ResetPasswordDto(string Email, string Token, string NewPassword);
 
